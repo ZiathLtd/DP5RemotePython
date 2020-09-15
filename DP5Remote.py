ae983123b86af01cf2a93d8b2b9bd8e794e5b593
neil-benn <neil.benn@ziath.com>
2020-09-15 21:23:33 +0100

    rerordered code

--- DP5Remote.py
+++ DP5Remote.py
@@ -2,7 +2,7 @@ import json
 import requests
 
 '''This class details some baic functions of the DP5 remote API using Python.  There is a swagger page for further documentation
-at http://<host>:<port>/swagger-ui.html.  
+at http://<host>:<port>/swagger-ui.html.
 
 Note that this uses the external requests module so install that by executing 'pip install reqeusts' before running the code'''
 class DP5Remote:
@@ -10,6 +10,14 @@ class DP5Remote:
     def __init__(self):
         self.__host = 'localhost'
         self.__port = 8777
+
+    @property
+    def __stub(self):
+        return f'http://{self.__host}:{self.__port}/dp5/remote/v1'
+
+    def __constructUrl(self, path):
+        url = f'{self.__stub}/{path}'
+        return url
 
     '''This is the host where DP5 is running'''
     @property
@@ -29,31 +37,24 @@ class DP5Remote:
     def host(self, port):
         self.__host__ = port
 
-    def __stub(self):
-        return f'http://{self.__host}:{self.__port}/dp5/remote/v1'
-
-    def __getUrl(self, path):
-        url = f'{self.__stub()}/{path}'
-        return url
-
     '''This is the version of DP5 whch is installed'''
     @property
     def version(self):
-        return requests.get(self.__getUrl('system/version')).json()
+        return requests.get(self.__constructUrl('system/version')).json()
 
     '''This is the status of the DP5 system, it can be IDLE, BUSY or ERROR'''
     @property
     def status(self):
-        return requests.get(self.__getUrl('system/status')).json()
+        return requests.get(self.__constructUrl('system/status')).json()
 
     '''This is the licence details of the DP5 registered user'''
     @property
     def licence(self):
-        return requests.get(self.__getUrl('licence')).json()
+        return requests.get(self.__constructUrl('licence')).json()
 
     '''This returns all the configured containers in the system'''
     def get_all_containers(self):
-        return requests.get(self.__getUrl('containers')).json()
+        return requests.get(self.__constructUrl('containers')).json()
 
     '''This will scan the container with the requested uid and return the results as JSON.  
     Note that errors are not handled.'''
@@ -65,15 +66,15 @@ class DP5Remote:
                   'raw_image' : str(retrieve_raw_image).lower(),
                   'annotated_image' : str(retrieve_raw_image).lower(),
                   '1dr2_image' : str(retrieve_1dr2_image).lower()}
-        return requests.post(self.__getUrl('scan'), params=params).json()
+        return requests.post(self.__constructUrl('scan'), params=params).json()
 
     def shutdown(self):
-        requests.put(self.__getUrl('system/shutdown'))
+        requests.put(self.__constructUrl('system/shutdown'))
 
 if __name__ == '__main__':
     
-    #create the dp5Remote object, note that this expects the DP5 service to be running
-    #if it is not then execute C:\Program Files\Ziath\DP5\resources\dp5-server\dp5-headles.exe 
+    #Create the dp5Remote object, note that this expects the DP5 service to be running
+    #if it is not then execute C:\Program Files\Ziath\DP5\resources\dp5-server\dp5-headless.exe 
     #(assuming you are on English language windows and using a default install location)
     dp5remote = DP5Remote()
     
